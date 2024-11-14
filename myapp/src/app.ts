2dbce606fc25fc4d7db81532ad5d177bee1b6bab
Aman Kumar <amankarguwal0@gmail.com>
2024-11-14 22:50:36 +0530

    add: integrate MongoDB connection and user creation functionality in app setup

--- myapp/src/app.ts
+++ myapp/src/app.ts
@@ -1,40 +1,89 @@
-import express from 'express'
-import cors from 'cors'
-import {errorMiddleware} from './middlewares/error.js'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import { errorMiddleware } from "./middlewares/error.js";
+import { console } from "inspector";
 
+dotenv.config({ path: "./.env" });
 
-  dotenv.config({path: './.env',});
+export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
+const port = process.env.PORT || 3000;
 
-  export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
-  const port = process.env.PORT || 3000;
+const app = express();
 
-
-  const app = express();
-
-
- app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors({origin:' * ',credentials:true}));
-app.use(morgan('dev')) 
-
-
-  app.get('/', (req, res) => {
-    res.send('Hello, World!');
+mongoose
+  .connect(process.env.MONGO_URI!, {
+    dbName: "DockerTut",
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-  // your routes here
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
 
-  
-  app.get("*", (req, res) => {
-    res.status(404).json({
-      success: false,
-      message: 'Page not found'
-    });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// your routes here
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Page not found",
+  });
+});
+
+app.use(errorMiddleware);
+
+app.listen(port, () =>
+  console.log("Server is working on Port:" + port + " in " + envMode + " Mode.")
+);
+
+const createUser = async (name: string, email: string) => {
+  const schema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   });
 
-  app.use(errorMiddleware);
-  
-  
-  app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
+  const User = mongoose.model("User", schema);
+
+  // const user = new User({
+  //   name,
+  //   email,
+  // });
+
+  // try {
+  //   const result = await user.save();
+  //   console.log(result + "User Created");
+  // } catch (err) {
+  //   console.log(err);
+  // }
+
+  await User.create({ name, email });
+
+  console.log("User Created");
+};
+
+// const createUsers = async () => {
+//   await createUser("John", "johnDoe3@gmail.com");
+// };
+
+// createUsers();
+
+// createUser("John", "johnDoe6@gmail.com");
