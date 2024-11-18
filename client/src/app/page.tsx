83820f233d832cc21df35fcb82b5fb1658f2c68f
Aman Kumar <amankarguwal0@gmail.com>
2024-11-18 16:47:05 +0530

    update: change background color from yellow to red in Home component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
               console.error(error);
             }
           }}
-          className="flex flex-col items-center gap-4 bg-yellow-400"
+          className="flex flex-col items-center gap-4 bg-red-400"
         >
           <input
             type="text"
